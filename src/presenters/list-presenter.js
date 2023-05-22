b7b2f84002e7f6ed116b45d58f4ad9c14fd938cb
AigerimYerk <aigerim@lessons.kz>
2023-05-22 10:50:22 +0600

    Created event listeners

--- src/presenters/list-presenter.js
+++ src/presenters/list-presenter.js
@@ -38,6 +38,11 @@ class ListPresenter extends Presenter {
       isSelected: point.offerIds.includes(it.id)
     }));
 
+    /**
+     *@type {UrlParams}
+    */
+    const urlParams = this.getUrlParams();
+
     return {
       id: point.id,
       types,
@@ -51,8 +56,44 @@ class ListPresenter extends Presenter {
       basePrice: point.basePrice,
       offers,
       isFavorite: point.isFavorite,
-      isEditable: index === 0,
+      isEditable: point.id === urlParams.edit
     };
+  }
+
+  /**
+   * @override
+   */
+
+  addEventListeners() {
+    /**
+     *
+     * @param {CustomEvent & {target: CardView}} event
+     */
+    const handleViewOpen = (event) => {
+
+      /**
+       *@type {UrlParams}
+       */
+      const urlParams = this.getUrlParams();
+
+      urlParams.edit = event.target.state.id;
+      this.setUrlParams(urlParams);
+    };
+
+    const handleViewClose = () => {
+
+      /**
+       *@type {UrlParams}
+       */
+      const urlParams = this.getUrlParams();
+
+      delete urlParams.edit;
+      this.setUrlParams(urlParams);
+
+    };
+
+    this.view.addEventListener('open', handleViewOpen);
+    this.view.addEventListener('close', handleViewClose);
   }
 }
 
