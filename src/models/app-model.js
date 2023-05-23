b1bd3b7cae8a42d2f3574d0c62f6558aedfe7cd3
AigerimYerk <aigerim@lessons.kz>
2023-05-23 09:32:59 +0600

    Added new type, created callback for sotring

--- src/models/app-model.js
+++ src/models/app-model.js
@@ -4,22 +4,37 @@ import destinations from '../data/destinations.json';
 import offerGroup from '../data/offers.json';
 
 class AppModel extends Model {
-  #points;
-  #destinations;
-  #offerGroup;
+  #points = points;
+  #destinations = destinations;
+  #offerGroup = offerGroup;
+  /**
+   *@type {Record<SortType, (a:Point, b:Point) => Number>}
+   */
+  #sortCallbackMap = {
+    day: (a, b) => Date.parse(a.startDateTime) - Date.parse(b.endDateTime),
+    event: () => 0,
+    time: (a, b) => AppModel.calcPointDuration(b) - AppModel.calcPointDuration(a),
+    price: (a, b) => a.basePrice - b.basePrice,
+    offers: () => 0,
+  };
 
-  constructor() {
-    super();
-    this.#points = points;
-    this.#destinations = destinations;
-    this.#offerGroup = offerGroup;
+  /**
+   * @param {number} point
+   */
+  static calcPointDuration(point) {
+    return Date.parse(point.endDateTime) - Date.parse(point.startDateTime);
   }
 
   /**
+  * @param {{sort?: SortType}} [criteria]
   * @return {Array<Point>}
   */
-  getPoints() {
-    return this.#points.map(AppModel.adaptPointForClient);
+  getPoints(criteria = {}) {
+
+    const adaptedPoints = this.#points.map(AppModel.adaptPointForClient);
+    const callBackSort = this.#sortCallbackMap[criteria.sort] ?? this.#sortCallbackMap.day;
+
+    return adaptedPoints.sort(callBackSort);
   }
 
 
