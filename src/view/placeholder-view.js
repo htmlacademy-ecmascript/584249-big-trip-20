8a0995afe057920e0f7261370364c669813200c2
AigerimYerk <aigerim@lessons.kz>
2023-06-12 13:57:49 +0600

    Added placeholder presenter

---
+++ src/view/placeholder-view.js
@@ -0,0 +1,33 @@
+import View from './view.js';
+import {html} from '../utils.js';
+
+/**
+ * @extends {View<PlaceholderViewState>}
+ */
+
+class PlaceholderView extends View {
+  constructor() {
+    super();
+
+    this.classList.add('placeholder-view');
+  }
+
+  /**
+   * @override
+   */
+  createHtml() {
+    const placeholder = this.state;
+
+    if (placeholder.isHidden) {
+      return '';
+    }
+
+    return html`
+    <p class="trip-events__msg">${placeholder.text}</p>
+    `;
+  }
+}
+
+customElements.define('placeholder-view', PlaceholderView);
+
+export default PlaceholderView;
